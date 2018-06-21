495f831ca11130d7ed0a56933bc38afe8609526f
Felipe Dutra Tine e Silva <tkanos@gmail.com>
2018-06-21 14:47:50 -0400

    Add Example of Httptrace

---
+++ examples/httptrace-basic/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptrace"
+	"os"
+	"time"
+
+	opentracing "github.com/opentracing/opentracing-go"
+	"github.com/ricardo-ch/go-tracing"
+)
+
+// this name is use to identify traces inside zipkin UI
+const (
+	appName = "my-application"
+)
+
+func main() {
+	tracing.SetGlobalTracer(appName, "http://localhost:9411")
+	defer tracing.FlushCollector()
+
+	doWork(context.Background())
+}
+
+func doWork(ctx context.Context) {
+	span, ctx := tracing.CreateSpan(ctx, "doWork", nil)
+	defer span.Finish()
+
+	r, err := googleRequest(ctx)
+	if err != nil {
+		os.Exit(-1)
+	}
+
+	option := func(trace *httptrace.ClientTrace, span opentracing.Span) {
+		trace.GotFirstResponseByte = func() {
+			span.LogKV("event", "Got First Response byte (changed)")
+		}
+	}
+
+	// Add The trace to the Request
+	r = tracing.InjectHTTPTraceSpan(r, option)
+	client := &http.Client{
+		Timeout: time.Duration(100) * (time.Millisecond),
+	}
+	client.Do(r)
+}
+
+func googleRequest(ctx context.Context) (r *http.Request, er error) {
+	r, er = http.NewRequest("GET", "http://www.google.ca/", nil) // ;)
+	if er != nil {
+		return nil, er
+
+	}
+	r = r.WithContext(ctx)
+
+	return r, nil
+}
