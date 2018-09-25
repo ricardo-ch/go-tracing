74c2b363c387d712b1aceb702bae26725cce4ed5
Francois Poinsot <gest.francois.poinsot@gmail.com>
2018-09-25 17:09:28 +0200

    try to keep zipkin logic but default to jaeger

--- tracer.go
+++ tracer.go
@@ -1,37 +1,77 @@
 package tracing
 
 import (
-	"io"
-
-	opentracing "github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
 )
 
-var globalCloser io.Closer
+var globalFlusher func()
 
 func GetGlobalTracer() opentracing.Tracer {
 	return opentracing.GlobalTracer()
 }
 
-func SetGlobalTracer() error {
-	cfg, err := jaegercfg.FromEnv()
-	if err != nil {
-		return err
-	} else {
-		var tracer opentracing.Tracer
-		tracer, globalCloser, err = cfg.New(
-			cfg.ServiceName,
-		)
-		if err != nil {
-			return err
-		}
-		opentracing.SetGlobalTracer(tracer)
+type TracerConfig struct {
+	TracingService string
+	AppName        string
+	TracingHost    string
+}
+
+type TracerOption func(config *TracerConfig)
+
+func UsingZipkin(appName string, host string) TracerOption {
+	return func(config *TracerConfig) {
+		config.TracingService = "zipkin"
+		config.TracingHost = host
+		config.AppName = appName
 	}
-	return err
+}
+
+func UsingJaeger() TracerOption {
+	return func(config *TracerConfig) {
+		config.TracingService = "jaeger"
+	}
+}
+
+func SetGlobalTracer(options ...TracerOption) error {
+	config := &TracerConfig{}
+	for _, option := range options {
+		option(config)
+	}
+
+	switch config.TracingService {
+	case "zipkin":
+		setGlobalZipkinTracer(config.AppName, config.TracingHost)
+	case "jaeger":
+	default:
+		setGlobalJaegerTracer()
+	}
+	return nil
 }
 
 func FlushCollector() {
-	if globalCloser != nil {
-		globalCloser.Close()
+	if globalFlusher != nil {
+		globalFlusher()
 	}
+}
+
+func setGlobalJaegerTracer() error {
+	cfg, err := jaegercfg.FromEnv()
+	if err != nil {
+		return err
+	}
+
+	tracer, globalCloser, err := cfg.NewTracer()
+	if err != nil {
+		return err
+	}
+	opentracing.SetGlobalTracer(tracer)
+
+	globalFlusher = func() {
+		if globalCloser != nil {
+			globalCloser.Close()
+		}
+	}
+
+	return err
 }
